ce5eb592779361944162f3629b269ec6f0041698
Qixiaoman <xiaoman_at@yeah.net>
2016-09-05 20:09:09 +0800

    初始化

---
+++ java/com/example/administrator/datademo/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.administrator.datademo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.textview)
+    TextView textView;
+    private Unbinder unbinder;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
+    }
+
+    @OnClick(R.id.textview)
+    public void Text() {
+        Toast.makeText(MainActivity.this, "字体",
+                Toast.LENGTH_SHORT).show();
+    }
+
+}
